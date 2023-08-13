12480b7643d5f31639b1757e4006e06ad1c85067
Goraniya Karan <karangoraniya@gmail.com>
2023-08-13 16:59:06 +0530

    chore: updated rpc for op and zora 🚀

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 require('dotenv').config();
 
-const { PRIVATE_KEY, POLYGON_SCAN_API_KEY } = process.env;
+const { PRIVATE_KEY, POLYGON_SCAN_API_KEY, OP_ALCHEMY_API } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
@@ -16,11 +16,11 @@ const config: HardhatUserConfig = {
       accounts: [`0x${PRIVATE_KEY}`],
     },
     op_testnet: {
-      url: 'https://rpc.ankr.com/optimism_testnet',
+      url: `https://opt-goerli.g.alchemy.com/v2/${OP_ALCHEMY_API}`,
       accounts: [`0x${PRIVATE_KEY}`],
     },
     zora_testnet: {
-      url: 'https://rpc.zora.energy/',
+      url: 'https://testnet.rpc.zora.energy',
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
