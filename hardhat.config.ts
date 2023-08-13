8dde75c0aabdcdfcd69713268709e83e681f0618
Goraniya Karan <karangoraniya@gmail.com>
2023-08-13 15:39:22 +0530

    feat: added different network configuration 🚀

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,32 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+require('dotenv').config();
+
+const { PRIVATE_KEY, POLYGON_SCAN_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  networks: {
+    mumbai: {
+      url: 'https://rpc.ankr.com/polygon_mumbai',
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mode: {
+      url: 'https://sepolia.mode.network/',
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    op_testnet: {
+      url: 'https://rpc.ankr.com/optimism_testnet',
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    zora_testnet: {
+      url: 'https://rpc.zora.energy/',
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: POLYGON_SCAN_API_KEY,
+  },
 };
 
 export default config;
